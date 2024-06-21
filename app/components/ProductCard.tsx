f19c63d49932081b3d385d7c558f9ff26704c52e
Khin Thandar Tun <106916866+KhinThandarTun@users.noreply.github.com>
2024-06-21 22:37:26 +0630

    routing

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddToCarts from './AddToCarts';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <AddToCarts />
+    </div>
+  )
+}
+
+export default ProductCard
