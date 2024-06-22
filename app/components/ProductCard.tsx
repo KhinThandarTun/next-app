5245c04574cf6b80cfb1ce2c0b6ec7ec801dc697
Khin Thandar Tun <106916866+KhinThandarTun@users.noreply.github.com>
2024-06-22 14:29:43 +0630

    fetching data

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import AddToCarts from './AddToCarts';
+import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
       <AddToCarts />
     </div>
   )
